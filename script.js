dce7ae8d15a8c0971f5924a2a2d4a2f76c263146
mercspring <68706508+mercspring@users.noreply.github.com>
2020-09-24 11:21:08 -0700

    added the function to collect ingredient info and recipes info from spoonacular, refactored the function to get nutrient info

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Global Varibles
-var recipes = [{title: "title", mealID:"", imageLink:"", instructions: "instructions", description: "description", servings:"", ingredients: ["1 slice cheese", "2 pieces of bread", "1 tablespoon butter"], totalDaily:{}, totalNutrients:{}, totalNutrientsKCal:{}}]
+// var recipes = [{title: "title", mealID:"", imageLink:"", instructions: "instructions", description: "description", servings:"", ingredients: ["1 slice cheese", "2 pieces of bread", "1 tablespoon butter"], totalDaily:{}, totalNutrients:{}, totalNutrientsKCal:{}}]
+var recipes = [];
+// var recipes = [{recipesInfo:{}, totalDaily:{}, totalNutrients:{}, totalNutrientsKCal:{}}]
 
 //test comment
